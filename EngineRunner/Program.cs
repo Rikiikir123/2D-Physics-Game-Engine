67eb9e9014482a54d40f7253794cc85d6641768f
Riste <rikisimonov@gmail.com>
2026-02-11 19:30:56 +0100

    Added EngineRunner proj

---
+++ EngineRunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Engine.Math;
+
+
+namespace EngineRunner
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vector2 position = new Vector2(10, 20);
+            //Console.WriteLine($"Vector position: X = {position.X}, Y = {position.Y}");
+            Console.WriteLine("Vector position: X =" + position.X + ", Y = " + position.Y);
+            Console.ReadLine();
+        }
+    }
+}
