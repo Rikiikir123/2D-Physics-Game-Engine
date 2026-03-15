d49afd5fe38049e96f30acc67b67441670495eeb
Riste <rikisimonov@gmail.com>
2026-03-15 22:30:24 +0100

    added AABB, added platform which interacts with the ball in a naive approach (currently just expects the ball from above)

---
+++ Engine/Physics/RAABB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Physics
+{
+    public struct RAABB
+    {
+        public float Left;
+        public float Right;
+        public float Top;
+        public float Bottom;
+
+        public RAABB(float left, float right, float top, float bottom)
+        {
+            this.Left = left;
+            this.Right = right;
+            this.Top = top;
+            this.Bottom = bottom;
+        }
+
+        public bool Intersects(RAABB other) // collision detection
+        {
+            return Left < other.Right &&    // check horizontal overlap
+                   Right > other.Left &&    // 
+                   Top < other.Bottom &&    // check vertical overlap
+                   Bottom > other.Top;      // 
+        }
+    }
+}
--- EngineRunner/Form1.cs
+++ EngineRunner/Form1.cs
@@ -12,11 +12,14 @@ namespace EngineRunner
     public partial class Form1 : Form
     {
         private RRigidBody body;
+
         private System.Windows.Forms.Timer timer;
         private Stopwatch stopwatch;
 
-        private float floorY;
+        private float clientHeight;
         private float clientWidth;
+
+        private RAABB platform;
 
         public Form1()
         {
@@ -28,8 +31,10 @@ namespace EngineRunner
 
             //Rigidbody (position, width, height, mass, useGravity)
             body = new RRigidBody(new RVector2(200, 200), 20f, 20f, 1f, true);
-            body.Velocity = new RVector2(0, -1000); // (x,y) pixels per second
-
+            body.Velocity = new RVector2(20000, 200000);          // direction toward (x,y) pixels per second
+            
+            //RAABB (left, right, top, bottom)
+            platform = new RAABB(150f, 350f, 250f, 270f);    
 
             stopwatch = new Stopwatch();
             stopwatch.Start();
@@ -48,10 +53,20 @@ namespace EngineRunner
             float deltaTime = currentTime - lastTime;
             lastTime = currentTime;
 
-            floorY = this.ClientSize.Height;
+            clientHeight = this.ClientSize.Height;
             clientWidth = this.ClientSize.Width;
 
-            body.Update(deltaTime, floorY, clientWidth);
+            body.Update(deltaTime, clientHeight, clientWidth);
+
+            // check if ball collides with platform
+            if (body.Bounds.Intersects(platform) && body.Velocity.Y > 0)
+            {
+                body.Position.Y = platform.Top - body.Height;
+                body.Velocity.Y *= -0.5f;
+                //CURRENTLY JUST EXPECTS THE BALL TO FALL FROM THE TOP
+                //TODO: MAKE THE ENGINE REMEMBER WHERE THE OBJECT CAME FROM 
+                //BEFORE THE COLLISION HAPPENED
+            }
 
             Invalidate(); // triggers redraw
         }
@@ -70,7 +85,15 @@ namespace EngineRunner
                 body.Height
             );
 
-            
+            e.Graphics.FillRectangle(
+                Brushes.Blue,
+                platform.Left,
+                platform.Top,
+                platform.Right - platform.Left,
+                platform.Bottom - platform.Top
+            );
+
+
         }
     }
 }
